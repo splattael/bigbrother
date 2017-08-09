adf7de264b1628fde23224381733a5d3f2416cdf
Peter Leitzen <pl@neopoly.de>
2017-08-09 12:26:48 +0200

    Show version via `-v` option

--- src/bigbrother/cli.cr
+++ src/bigbrother/cli.cr
@@ -1,6 +1,8 @@
 require "option_parser"
 require "yaml"
 
+require "./version"
+
 module Bigbrother
   module Cli
     def self.run(argv)
@@ -8,6 +10,10 @@ module Bigbrother
 
       parser = OptionParser.parse(argv) do |parser|
         parser.banner = "Usage: bigbrother -c config.yml [arguments]"
+        parser.on("-v", "--version", "Show current version") do
+          puts version
+          exit 0
+        end
         parser.on("-c YAML", "--config=YAML", "Provide config file") do |name|
           config_file = name
         end
@@ -22,6 +28,16 @@ module Bigbrother
 
       config = Config.from_yaml(File.read(config_file.not_nil!))
       App.new(config).run
+    end
+
+    def self.version
+      "%{name} %{version} [%{sha1}] (%{date}) Crystal %{cr_version}" % {
+        name: "bigbrother",
+        version: VERSION,
+        sha1: VERSION_SHA1,
+        date: VERSION_DATE,
+        cr_version: Crystal::VERSION
+      }
     end
   end
 end
--- src/bigbrother/version.cr
+++ src/bigbrother/version.cr
@@ -1,3 +1,5 @@
 module Bigbrother
   VERSION = "0.1.0"
+  VERSION_SHA1 = {{ `git rev-parse --short HEAD`.stringify.chomp }}
+  VERSION_DATE = {{ `date +%F`.stringify.chomp }}
 end
