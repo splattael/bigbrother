7020b17c70a4482140c045bccaa2109ae173b4dd
Peter Leitzen <pl@neopoly.de>
2017-11-04 15:59:35 +0100

    Fallback to "???????" if we cannot determine VERSION_SHA1

--- src/bigbrother/version.cr
+++ src/bigbrother/version.cr
@@ -1,5 +1,5 @@
 module Bigbrother
   VERSION      = "0.1.0"
-  VERSION_SHA1 = {{ `git rev-parse --short HEAD`.stringify.chomp }}
+  VERSION_SHA1 = {{ `git rev-parse --short HEAD 2>/dev/null || echo "??????"`.stringify.chomp }}
   VERSION_DATE = {{ `date +%F`.stringify.chomp }}
 end
