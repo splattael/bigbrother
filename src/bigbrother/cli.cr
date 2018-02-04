b642c5ea667addcbfa6f56209a72de75358be1fe
Peter Leitzen <pl@neopoly.de>
2018-02-04 11:07:01 +0100

    Revert "Reload config without restart via SIGHUP"
    This reverts commit cbcf3df909486386569b5d23d203b81c81ba1336.
    
    Reloading fails because it's not easy to unbind ports in Telegram
    notifier. Currently, it would be impossible to interrupt polling as well.

--- src/bigbrother/cli.cr
+++ src/bigbrother/cli.cr
@@ -5,46 +5,7 @@ require "base64"
 require "./version"
 
 module Bigbrother
-  class Cli
-    enum Status
-      Run
-      Reload
-      Stop
-    end
-
-    @config_file : String?
-    @webhook_port : Int32 = 0
-    @parser : OptionParser
-    @status : Status = Status::Run
-
-    def initialize(@config_file, @webhook_port, @parser)
-    end
-
-    def run
-      while @status != Status::Stop
-        start_app
-      end
-    end
-
-    private def start_app
-      config = read_config(@config_file, ENV["BB_CONFIG_YAML_BASE64"]?)
-
-      unless config
-        message = "Unable to read or parse config\n\n" + @parser.to_s
-        abort message
-      end
-
-      if @webhook_port > 0
-        configure_port(config, @webhook_port)
-      end
-
-      @status = Status::Run
-
-      app = App.new(config.not_nil!)
-      register_signal_handlers(app)
-      app.run
-    end
-
+  module Cli
     def self.run(argv)
       config_file = nil
       webhook_port = 0
@@ -66,11 +27,22 @@ module Bigbrother
         end
       end
 
-      cli = new(config_file, webhook_port, parser)
-      cli.run
+      config = read_config(config_file, ENV["BB_CONFIG_YAML_BASE64"]?)
+
+      unless config
+        abort parser.to_s
+      end
+
+      if webhook_port > 0
+        configure_port(config, webhook_port)
+      end
+
+      app = App.new(config.not_nil!)
+      register_signal_handlers(app)
+      app.run
     end
 
-    private def read_config(config_file, yaml_string)
+    private def self.read_config(config_file, yaml_string)
       if config_file && File.exists?(config_file)
         Config.from_yaml(File.read(config_file))
       elsif yaml_string
@@ -78,17 +50,17 @@ module Bigbrother
       end
     end
 
-    private def configure_port(config : Config, port)
+    private def self.configure_port(config : Config, port)
       config.notifiers.each { |notifier| configure_port(notifier, port) }
     end
 
-    private def configure_port(config : Notifier::Telegram, port)
+    private def self.configure_port(config : Notifier::Telegram, port)
       if webhook = config.webhook
         webhook.port = port
       end
     end
 
-    private def configure_port(config : Notifier, port)
+    private def self.configure_port(config : Notifier, port)
       # fallback
     end
 
@@ -102,22 +74,11 @@ module Bigbrother
       }
     end
 
-    private def register_signal_handlers(app)
-      handle_signal(Signal::INT, Signal::TERM, message: "Exit") { stop!(app) }
-      handle_signal(Signal::HUP, message: "Reloading config") { reload!(app) }
+    private def self.register_signal_handlers(app)
+      handle_signal(Signal::INT, Signal::TERM, message: "Exit") { app.stop }
     end
 
-    private def stop!(app)
-      @status = Status::Stop
-      app.stop
-    end
-
-    private def reload!(app)
-      @status = Status::Reload
-      app.stop
-    end
-
-    private def handle_signal(*signals, message, &block)
+    private def self.handle_signal(*signals, message, &block)
       signals.each do |signal|
         signal.trap do
           puts "Caught signal #{signal} -> #{message}"
