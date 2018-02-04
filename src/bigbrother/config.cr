efd5d9798fd0deee415c1f43e7411530d63cc22b
Peter Leitzen <pl@neopoly.de>
2018-02-04 10:30:40 +0100

    Crystal format

--- src/bigbrother/config.cr
+++ src/bigbrother/config.cr
@@ -5,8 +5,8 @@ module Bigbrother
     YAML.mapping(
       check_every: Int32,
       retries: {
-        type: Int32,
-        default: 0i32
+        type:    Int32,
+        default: 0i32,
       },
       notifiers: Array(Notifier),
       checks: Array(Check)
