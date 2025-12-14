f234a0f6269fb64a5e52d625b3e5d7d0702b668b
Peter Leitzen <peter@leitzen.de>
2025-12-14 09:27:15 +0100

    Check minimal SSL cert expiry defaulting to 7 days

--- src/bigbrother/check/http.cr
+++ src/bigbrother/check/http.cr
@@ -1,9 +1,12 @@
 require "http/client"
+require "openssl"
 
 module Bigbrother
   module Check
     class Http
       include Check
+
+      @cert_expires_at : Time?
 
       enum Method
         GET
@@ -54,16 +57,45 @@ module Bigbrother
           type:    Int32,
           nilable: true,
           default: 120,
+        },
+        ssl_min_days_valid: {
+          type: Int32,
+          nilable: true,
+          default: 7
         }
 
       def label
-        "#{http_method} #{@url}"
+        if @cert_expires_at
+          "#{http_method} #{@url} cert_expires_at=#{@cert_expires_at}"
+        else
+          "#{http_method} #{@url}"
+        end
       end
 
       def check
         uri = URI.parse(@url)
         uri = URI.parse("http://#{@url}") unless uri.scheme
 
+        match_not_after_validity(uri) if uri.scheme == "https" && @ssl_min_days_valid
+        match_http_body(uri)
+      end
+
+      private def match_not_after_validity(uri)
+        hostname = uri.host || "unknown"
+        port = uri.port || 443
+
+        context = OpenSSL::SSL::Context::Client.new
+        tcp_socket = TCPSocket.new(hostname, port)
+        ssl_socket = OpenSSL::SSL::Socket::Client.new(tcp_socket, context, hostname: hostname)
+        cert = ssl_socket.peer_certificate
+        @cert_expires_at = cert.not_after
+
+        if @cert_expires_at.not_nil! - Time::Span.new(days: @ssl_min_days_valid.not_nil!) < Time.utc
+          fail "SSL certificate expires in < #{@ssl_min_days_valid} days"
+        end
+      end
+
+      private def match_http_body(uri)
         HTTP::Client.new(uri) do |client|
           client.dns_timeout = @dns_timeout.not_nil!.seconds
           client.connect_timeout = @connect_timeout.not_nil!.seconds
